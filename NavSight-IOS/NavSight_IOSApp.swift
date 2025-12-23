0eaaa05da15dcf14172c30acde266be33b9b539d
Gurnoor Singh Pannu <gurnxxrpannu@gmail.com>
2025-12-23 17:50:35 +0530

    Initial Commit

---
+++ NavSight-IOS/NavSight_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavSight_IOSApp.swift
+//  NavSight-IOS
+//
+//  Created by Gurnoor Singh Pannu on 23/12/25.
+//
+
+import SwiftUI
+
+@main
+struct NavSight_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NavSight-IOSTests/NavSight_IOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  NavSight_IOSTests.swift
+//  NavSight-IOSTests
+//
+//  Created by Gurnoor Singh Pannu on 23/12/25.
+//
+
+import Testing
+@testable import NavSight_IOS
+
+struct NavSight_IOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
